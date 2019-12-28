70ade197f2ef7680d628687b5cca1fe2c8b2d5d3
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-28 02:45:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
   fresh_cart = []
   counter = 0
 while counter > cart.length
-fresh_cart_item = find_item_by_name_in_collection(cart[counter][:item])
+fresh_cart_item = find_item_by_name_in_collection(cart[counter][:item], fresh_cart)
 counter += 1
 end
   
