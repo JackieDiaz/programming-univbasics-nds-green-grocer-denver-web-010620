895336273afbd95026ad89ba6fcd40496de020f1
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-28 02:51:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,8 @@ if fresh_cart_item != nil
 else 
   fresh_cart_item = {
     :item => cart[counter][:item],
-    :price => cart[counter][:price]
+    :price => cart[counter][:price],
+    :clearance 
   }
 counter += 1
 end
