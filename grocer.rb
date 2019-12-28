8acf2d4017feafe8f79ee426bf297a71e6dec9ed
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-28 03:28:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ while counter < coupons.length
       cart_item_with_coup = {
         :item => coup_name,
         :price => coupons[counter][:cost] / coupons[counter][:num]
-        :count => coupons[counter][]
+        :count => coupons[counter][:num]
       }
     end
   end
