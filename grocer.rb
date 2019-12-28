c2e61f1ff4891d63a79a430f12b566cf9cffa039
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-28 03:29:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ while counter < coupons.length
         :item => coup_name,
         :price => coupons[counter][:cost] / coupons[counter][:num],
         :count => coupons[counter][:num],
-        
+        :clearance => 
       }
     end
   end
