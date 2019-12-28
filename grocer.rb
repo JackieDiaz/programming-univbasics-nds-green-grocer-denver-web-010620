ddd9de02ee0253c5bec5c5523b78af49fd6682f8
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-28 02:30:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   counter = 0
   while counter < collection.length 
- if collection
+ if collection[counter]
   collection[counter]
   counter +=1
 end
